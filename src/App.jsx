c7180277d4aee81ee030e7104695a8dad8daa109
Али <lal4topol@yandex.ru>
2023-10-05 21:49:50 +0300

    release v4

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useEffect } from "react";
 
 import Header from "./assets/components/Header";
 import Content from "./assets/components/Content";
 import Buy from "./assets/components/Buy";
 import Footer from "./assets/components/Footer";
+
+
 const App = () => {
+
+  useEffect(() => {
+    document.body.style.overflow = "hidden";
+  }, []);
+
+
   return (
     <div className="container">
       <Header></Header>
