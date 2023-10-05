db74b8fd0bf7fbf94769770c44e7f39f11a75428
Али <lal4topol@yandex.ru>
2023-10-05 20:28:38 +0300

    added slider

--- src/assets/components/Exclusive.jsx
+++ src/assets/components/Exclusive.jsx
@@ -5,22 +5,51 @@ import excFirst from '../images/1.jpeg'
 import excSecond from '../images/2.jpeg'
 import excThird from '../images/3.jpeg'
 import excFourth from '../images/4.jpeg'
+import prevSlideIcon from '../icons/prev-slide.svg'
+import nextSlideIcon from '../icons/next-slide.svg'
 import { useResize } from '../hooks/useResize'
 import { useState } from 'react'
 
 
 const Exclusive = () => {
   const sliderImgList = [excFirst,excSecond,excThird,excFourth]
-  const [sliderImage, setSliderImage] = useState(sliderImgList[0])
-  let counter = 0
+  const [sliderImage, setSliderImage] = useState(0)
 
+  const [prevOpacity, setPrevOpacity] = useState(0.5)
+  const [nextOpacity, setNextOpacity] = useState(1)
 
   function sliderNext() {
-    if(counter<3) {
-      counter++
-      setSliderImage(sliderImgList[counter])
+    if(sliderImage<3) {
+      setSliderImage(sliderImage + 1)
     }
-  }
+
+    if(sliderImage>=0) {
+      setPrevOpacity(1)
+    }else {
+      setPrevOpacity(0.5)
+    }
+
+    if(sliderImage>1) {
+      setNextOpacity(0.5)
+    }
+}
+
+
+  function sliderPrev() {
+    if(sliderImage>0) {
+      setSliderImage(sliderImage - 1)
+    } 
+
+    if(sliderImage>1) {
+      setPrevOpacity(1)
+    }else {
+      setPrevOpacity(0.5)
+    }
+    if(sliderImage<=3) {
+      setNextOpacity(1)
+    }
+
+}
 
   const { width, isScreenSm, isScreenMd, isScreenLg, isScreenXl } = useResize();
   return (
@@ -57,13 +86,13 @@ const Exclusive = () => {
       ) : (
         <>
           <div className="slider">
-            <div className="slider-decoration"></div>
+            <div className="slider-decoration"><img src={firstDecore} alt="decoration" /></div>
             <div className="slider-main-image">
-              <img src={sliderImage} alt="" width={344}/>
+              <img src={sliderImgList[sliderImage]} alt="" width={344}/>
             </div>
             <div className="slider-buttons">
-              <button style={{marginRight:"12px"}}>prev</button>
-              <button onClick={sliderNext}>next</button>
+              <button style={{marginRight:"12px"}}><img style={{opacity:`${prevOpacity}`}} onClick={sliderPrev} src={prevSlideIcon} alt='прошлый слайд'></img></button>
+              <button><img onClick={sliderNext} src={nextSlideIcon} alt="следующий слайд" style={{opacity:`${nextOpacity}`}}/></button>
             </div>
           </div>
         </>
