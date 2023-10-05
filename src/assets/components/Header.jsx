d49339a645d513a155c8cb2b98a0f844456c1b64
Али <lal4topol@yandex.ru>
2023-10-05 13:13:15 +0300

    update

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -17,7 +17,10 @@ const Header = () => {
                     <img className="join-soc"src={joinSoc} alt="Вступить в сообщество" />
                 </div>
                 <div className='buy-aboniment'>
-                    <p>Купите абонемент, чтобы стать<br />частью клуба в новом сезоне–2024.<br />8 месяцев за <span className='aboniment-new-price'>499 ₽</span><span className='aboniment-old-price'>3190 ₽</span></p>
+                    <p>Купите абонемент, чтобы стать<br />частью клуба в новом сезоне–2024.<br />8 месяцев за <span className='aboniment-price'><span className='aboniment-price-new-price'>499 ₽</span> <span className='aboniment-price-old-price'>3190₽</span></span></p>
+                </div>
+                <div className="join-club-button">
+                    <button>Стать частью клуба</button>
                 </div>
             </div>
         </header>
