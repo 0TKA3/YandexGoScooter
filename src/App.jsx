7aa97831ecbcf486bed6efcfad6e26787d11f0f9
Али <lal4topol@yandex.ru>
2023-10-05 14:01:37 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 import Header from "./assets/components/Header";
-
+import Content from "./assets/components/Content";
 
 const App = () => {
   return (
     <div className="container">
       <Header></Header>
+      <Content></Content>
     </div>
   );
 }
